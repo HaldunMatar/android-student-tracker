8b0992b36caff2b92680c60c3f92f1da35799a41
unknown <kh82matar@gmail.com>
2021-03-04 16:27:04 +0300

    basic RecyclView and evlation

---
+++ app/src/main/java/com/haldun/android/student/studentTracker/BindingUtils.kt
@@ -0,0 +1,25 @@
+
+
+package com.haldun.android.student.studentTracker
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.haldun.android.student.R
+import com.haldun.android.student.database.Student
+
+
+@BindingAdapter("studentImage")
+fun ImageView.setSleepImage(item: Student?) {
+    item?.let {
+        setImageResource(when (item.studentId) {
+            item.studentId % 10 -> R.drawable.ic_sleep_0
+            item.studentId % 10 -> R.drawable.ic_sleep_1
+            item.studentId % 10 -> R.drawable.ic_sleep_2
+            item.studentId % 10 -> R.drawable.ic_sleep_3
+            item.studentId % 10 -> R.drawable.ic_sleep_4
+            item.studentId % 10 -> R.drawable.ic_sleep_5
+            else -> R.drawable.ic_sleep_active
+        })
+    }
+}
